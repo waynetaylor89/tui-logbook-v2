c167f7ed3c9e10b0eb0055788f51c60975705710
Wayne <your@email.com>
2026-05-11 11:01:03 +0100

    Test Zustand store import to identify white screen cause
    - Import useLogbookStore to test if it causes the white screen
    - Connect login functionality to the store
    - This will determine if the Zustand persistence is the issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import Login from "./components/Login.jsx";
+import useLogbookStore from "./store/useLogbookStore.js";
 
 export default function AircraftMovementLogbook() {
   const [currentUser, setCurrentUser] = useState(null);
+  const { login } = useLogbookStore();
   
   console.log("App component is rendering!");
   
   if (!currentUser) {
-    return <Login onLogin={() => setCurrentUser("test")} />;
+    return <Login onLogin={(username, password) => {
+      login(username, password).then(success => {
+        if (success) setCurrentUser(username);
+      });
+    }} />;
   }
   
   return (
